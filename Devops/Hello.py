94d030033ce20c699ea0f4f5de9ed168c16ed279
Ranjith  Kandukuri <ranjithkandukuri@Ranjiths-MacBook-Pro.local>
2017-12-10 20:44:25 -0800

    Adding hello python script. Input, if, elif, else

---
+++ Devops/Hello.py
@@ -0,0 +1,9 @@
+print("Hello Ranjith")
+first_name = input("First Name? ")
+sib_num = input("How many siblings do you have? ")
+if(first_name == "Ranjith" and sib_num == 1):
+    print("You are Ranjith Kandukuri")
+elif(first_name == "Rohith"):
+    print("You are Rohith Kandukuri")
+else:
+    print("You are not Kanudukuri")
