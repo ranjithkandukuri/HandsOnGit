63d90b0acaf401244734aaf26a527735e3a104c6
Ranjith  Kandukuri <ranjithkandukuri@Ranjiths-MacBook-Pro.local>
2017-12-10 21:15:40 -0800

    Added function and not equals operator

--- Devops/Hello.py
+++ Devops/Hello.py
@@ -1,9 +1,11 @@
-print("Hello Ranjith")
-first_name = input("First Name? ")
-sib_num = input("How many siblings do you have? ")
-if(first_name == "Ranjith" and sib_num == 1):
-    print("You are Ranjith Kandukuri")
-elif(first_name == "Rohith"):
-    print("You are Rohith Kandukuri")
-else:
-    print("You are not Kanudukuri")
+def helloRanjith():
+    print("Hello Ranjith")
+    first_name = input("First Name? ")
+    sib_num = input("How many siblings do you have? ")
+    if(first_name == "Ranjith" and sib_num == 1):
+        print("You are Ranjith Kandukuri")
+    elif(first_name == "Ranjith" and sib_num != 1):
+        print("You are Ranjith but not Kandukuri")
+    else:
+        print("You are not Kanudukuri")
+helloRanjith()
