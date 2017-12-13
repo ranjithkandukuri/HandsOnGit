08a4815a96908e2aa8961c05a2d0bcd5d961c9e9
Ranjith  Kandukuri <ranjithkandukuri@Ranjiths-MacBook-Pro.local>
2017-12-12 22:19:55 -0800

    Adding Calculator python script with while, try, except

---
+++ Devops/Calculator.py
@@ -0,0 +1,50 @@
+def add(num1,num2):
+    """Returns num1 plus num2"""
+    return num1 + num2
+
+def sub(num1,num2):
+    """Returns num1 minus num2"""
+    return num1 - num2
+
+def mul(num1,num2):
+    """Returns num1 times num2"""
+    return num1 * num2
+
+def div(num1,num2):
+    """Returns num1 divided by num2"""
+    return num1 / num2
+
+def main():
+    valid_input = False
+    while not valid_input:
+        """Allows user to run basic calculator with two numbers"""
+        try:
+            num1 = int(input("What is number 1? "))
+            num2 = int(input("What is number 2? "))
+            operation = int(input("Give the number of the operation you want to do. \n1.Addition \n2.Subtraction \n3.Multiplication \n4.Division\n"))
+            valid_input = True
+        except:
+            try_again = input("Invalid input. Do you want to try again (y/n)?")
+            if (try_again == 'n'):
+                print("Exiting the program. Hasta la vista!")
+                return
+            else:
+                print("\n")
+
+
+    if (operation == 1):
+        print(add(num1,num2))
+
+    elif (operation == 2):
+        print(sub(num1,num2))
+
+    elif (operation == 3):
+        print(mul(num1,num2))
+
+    elif (operation == 4):
+        print(div(num1,num2))
+
+    else:
+        print("Invalid operation")
+
+main()
---
+++ Devops/test.py
@@ -0,0 +1,19 @@
+num1 = raw_input("Give a number: ")
+num2 = raw_input("Give another number: ")
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+def main():
+    operation = input("What do you want? (+,-,*,/)")
+    if(operation == "+"):
+        print(add)
+    elif(operation == "-"):
+        print(sub)
+    elif(operation == "*"):
+        print(mul)
+    elif(operation == "/"):
+        print(div)
+    else:
+        print("invalid operator")
+main()
